84c97108e3c6fce5454f075626d876df14b4ccdb
Floris Douw <fdouw@hotmail.com>
2023-12-18 22:38:55 +0100

    Add day 18
    Complete rewrite from part 1: using sets is impossible for part 2, so use a formula from https://mathopenref.com/coordpolygonarea.html
    Use complex numbers instead of tuples, because adding and multiplying is so much easier this way.
    
    The discreteness of the problem means some tweaks are needed to count the boundary properly: if you image the vertices being on the top left corner, then the top and left boundaries are counted, but not the bottom and right. The final +1 is probably the bottom right corner in this scenario, but I'm not sure.

--- 2023/day18.py
+++ 2023/day18.py
@@ -1,78 +1,46 @@
 #!/usr/bin/env python
 
-
-from queue import Queue
-
-
-dirs = (0 - 1j, 0 + 1j, -1 + 0j, 1 + 0j)
+from itertools import pairwise
 
 
-# def print_dig(labels=True):
-#     if labels:
-#         print(" ", end="")
-#         print("".join(str(x % 10) for x in range(min_x, max_x + 1)))
-#     for y in range(min_y, max_y + 1):
-#         if labels:
-#             print(y % 10, end="")
-#         for x in range(min_x, max_x + 1):
-#             z = complex(x, y)
-#             if z in dig:
-#                 print("#", end="")
-#             elif z in outside:
-#                 print("O", end="")
-#             else:
-#                 print(".", end="")
-#         if labels:
-#             print(y % 10)
-#     if labels:
-#         print(" ", end="")
-#         print("".join(str(x % 10) for x in range(min_x, max_x + 1)))
-#     print()
+def area(vertices: [complex]) -> int:
+    """Computes the area of a polygon from its vertices
+
+    vertices: list of complex numbers"""
+    # See: https://mathopenref.com/coordpolygonarea.html
+
+    # Count the inside and the boundary itself
+    # not sure why the boundary needs to be halved, or why we're off by 1... probably because this is a discrete problem
+    # and if you imagine the vertices on the upper left corner of the squares, then half the boundary is counted and the
+    # other isn't.
+    vertices.append(vertices[0])
+    inside = abs(
+        sum(int(a.real * b.imag - a.imag * b.real) for a, b in pairwise(vertices)) // 2
+    )
+    boundary = sum(int(abs(a - b)) for a, b in pairwise(vertices)) // 2
+    return inside + boundary + 1
+
+
+dirs = (1 + 0j, 0 + 1j, -1 + 0j, 0 - 1j)
+pos1 = 0 + 0j
+pos2 = 0 + 0j
+vertices1 = [pos1]
+vertices2 = [pos2]
 
 
 with open("inputs/day18", "r") as f:
-    dig = set()
-    position = 0 + 0j
     for line in f.readlines():
         direction, distance, hex = line.split()
-        step = dirs["UDLR".index(direction)]
-        for _ in range(int(distance)):
-            position += step
-            dig.add(position)
+        # step 1
+        step = dirs["RDLU".index(direction)]
+        pos1 += int(distance) * step
+        vertices1.append(pos1)
+        # part 2
+        distance = int(hex[2:7], 16)
+        step = dirs[int(hex[-2])]
+        pos2 += int(distance) * step
+        vertices2.append(pos2)
 
 
-def measure_inside(dig: set[complex]) -> int:
-    # Take a boundary of 1m around the min and max x and y, this boundary is definitely outside the loop and also
-    # guarantees that all the outside areas are connected(?) Then do a flood fill to determine the area outside the loop
-    x_vals = set(int(z.real) for z in dig)
-    y_vals = set(int(z.imag) for z in dig)
-    min_x = min(x_vals) - 1
-    max_x = max(x_vals) + 2  # max is 1 more, because half open ranges
-    min_y = min(y_vals) - 1
-    max_y = max(y_vals) + 2  # idem
-    total_area = (max_x - min_x) * (max_y - min_y)
-
-    seen = set()
-    outside = 0
-    queue = Queue()
-    queue.put(complex(min_x, min_y))
-    seen.add(complex(min_x, min_y))
-
-    while not queue.empty():
-        item = queue.get()
-        outside += 1
-        for step in dirs:
-            nb = item + step
-            if (
-                nb not in seen
-                and nb not in dig
-                and min_x <= nb.real < max_x
-                and min_y <= nb.imag < max_y
-            ):
-                queue.put(nb)
-            seen.add(nb)
-
-    return total_area - outside
-
-
-print(f"Part 1: {measure_inside(dig)}")
+print(f"Part 1: {area(vertices1)}")
+print(f"Part 2: {area(vertices2)}")
