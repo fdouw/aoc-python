01fcb5ee4bca9c601dc8092de5b07498c60a4dc2
Floris Douw <fdouw@hotmail.com>
2023-12-21 00:26:20 +0100

    Add day 20, part 2: some guess work that there are cycles, code needs cleaning up

--- 2023/day20.py
+++ 2023/day20.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-from queue import Queue
+from collections import deque
+from itertools import pairwise
+from math import lcm
 
 
 LO = False
@@ -16,7 +18,7 @@ class ConjunctionModule:
     def process(self, source: str, signal: bool):
         self.memory[source] = signal
         out_signal = not all(self.memory.values())
-        return [(self.name, next_module, out_signal) for next_module in self.outputs]
+        return ((self.name, next_module, out_signal) for next_module in self.outputs)
 
     def connect_input(self, input: str):
         self.memory[input] = False
@@ -31,12 +33,12 @@ class FlipFlopModule:
     def process(self, _source: str, signal: bool):
         if not signal:
             self.state = not self.state
-            return [
+            return (
                 (self.name, next_module, self.state) for next_module in self.outputs
-            ]
+            )
         else:
             # Return empty list: caller expects something
-            return []
+            return ()
 
     def connect_input(self, input: str):
         pass
@@ -48,7 +50,7 @@ class BroadcastModule:
         self.outputs = output_str.split(", ")
 
     def process(self, _source: str, signal: bool):
-        return [(self.name, next_module, signal) for next_module in self.outputs]
+        return ((self.name, next_module, signal) for next_module in self.outputs)
 
     def connect_input(self, input: str):
         pass
@@ -69,34 +71,54 @@ with open("inputs/day20", "r") as f:
             raise f"Could not interpret module '{line}'"
 
 # Make sure the Conjunctions know their inputs, quadratic unfortunately
+rx_input = ""
 for name, module in modules.items():
     for output in module.outputs:
         if output in modules:
             modules[output].connect_input(name)
+        elif output == "rx":
+            rx_input = name
+            # print(f"direct input: {name}")
+
+rx_inputs = {}
+for name, module in modules.items():
+    if rx_input in module.outputs:
+        # print(f"input[{name}] = {modules[name]}")
+        rx_inputs[name] = []
+
 
 # Run the program
 counts = [0, 0]
-signal_queue = Queue()
+signal_queue = deque()
 
 part2 = 0
-for cycle in range(1_000_000_000):
-    signal_queue.put(("button", "broadcaster", False))
+for cycle in range(10_000):
+    signal_queue.append(("button", "broadcaster", False))
 
-    while not signal_queue.empty():
-        source, dest, signal = signal_queue.get()
-        # print(f"{source} -{signal}-> {dest}")
+    while signal_queue:
+        source, dest, signal = signal_queue.popleft()
         if cycle < 1000:
             counts[signal] += 1
         if dest == "rx" and not signal:
             part2 = cycle
+            break
+        elif source in rx_inputs and signal is True:
+            rx_inputs[source].append(cycle)
+            if all(len(x) >= 2 for x in rx_inputs.values()):
+                # Assume there are cycles
+                break
 
         if dest in modules:
             for new_signal in modules[dest].process(source, signal):
-                signal_queue.put(new_signal)
-
-    if cycle >= 1000 and part2 > 0:
-        break
+                signal_queue.append(new_signal)
 
 part1 = counts[0] * counts[1]
 
+# for name, hist in rx_inputs.items():
+#     print(f"{name}: {hist[:20]} -- {[b-a for a,b in pairwise(hist[:20])]}")
+
+part2 = lcm(*[hist[1] - hist[0] for hist in rx_inputs.values()])
+
+
 print(f"Part 1: {part1}")
+print(f"Part 2: {part2}")
