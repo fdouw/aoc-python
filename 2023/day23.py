7ce67620762a6ca30b9b291b46fe36a44a8a951e
Floris Douw <fdouw@hotmail.com>
2023-12-23 15:52:31 +0100

    Add improved version for day 23, part 2

--- 2023/day23.py
+++ 2023/day23.py
@@ -71,6 +71,78 @@ class Tile:
                 return set()
 
 
+class BidirectionalTile:
+    def __init__(self, x: int, y: int, is_start: bool = False, is_end: bool = False):
+        self.x = x
+        self.y = y
+        self.adjacent = []
+        self.visited = False
+        self.is_start = is_start
+        self.is_end = is_end
+
+    def is_node(self) -> bool:
+        """If a tile is connected to exactly 2 other tiles, it is part of a path and could be ignored. Otherwise it is
+        either connected to 1 tile (endpoint) or more than 2 (junction).
+
+        Returns True iff this tile is a node, ie a junction or an endpoint."""
+        return len(self.adjacent) != 2
+
+    def connect(self, other):
+        """Connects this tile to other.
+
+        NB: if other is already connected, they will be connected a second time!"""
+        self.adjacent.append((1, other))
+        other.adjacent.append((1, self))
+
+    def join(self):
+        """Assumes there are exactly 2 other tiles connected to this one. Joins those 2 directly, while retaiting the
+        length of the path between them."""
+        len_a, tile_a = self.adjacent[0]
+        len_b, tile_b = self.adjacent[1]
+        tile_a._replace((len_a, self), (len_a + len_b, tile_b))
+        tile_b._replace((len_b, self), (len_a + len_b, tile_a))
+
+    def _replace(self, other, new):
+        """Looks for other among its adjacent tiles and replaces it with new.
+
+        other: (int, BidirectionalTile), adjacent to self
+        new: (int, BidirectionalTile), tuple of new distance and new tile"""
+        idx = self.adjacent.index(other)
+        self.adjacent[idx] = new
+
+    def find_path(self) -> int:
+        """Recursively finds the longest path to the end tile via DFS and returns the length of the path."""
+        if self.is_end:
+            return 1
+        else:
+            self.visited = True
+            path_len = 0
+            for dist, next in self.adjacent:
+                if not next.visited:
+                    d = next.find_path()
+                    if d > 0:
+                        path_len = max(path_len, dist + d)
+            self.visited = False
+            return path_len
+
+    def trace_path(self):
+        """Recursively finds the longest path to the end tile via DFS and returns the tiles comprising the path.
+        Overcounts the length by 1."""
+        if self.is_end:
+            return (1, {self})
+        else:
+            current = (0, set())
+            self.visited = True
+            for dist, next in self.adjacent:
+                if not next.visited:
+                    d, next_path = next.trace_path()
+                    if d > 0 and dist + d > current[0]:
+                        next_path.add(self)
+                        current = (dist + d, next_path)
+            self.visited = False
+            return current
+
+
 with open("inputs/day23", "r") as f:
     tiles = {}
     tiles2 = {}
@@ -84,7 +156,7 @@ with open("inputs/day23", "r") as f:
             if tile != "#":
                 # Assume only start on the first line
                 last_tile = Tile(x, y, tile, is_start=(y == 0))
-                last_tile2 = Tile(x, y, tile, is_start=(y == 0))
+                last_tile2 = BidirectionalTile(x, y, is_start=(y == 0))
                 tiles[(x, y)] = last_tile
                 tiles2[(x, y)] = last_tile2
                 above = (x, y - 1)
@@ -92,20 +164,23 @@ with open("inputs/day23", "r") as f:
                 if above in tiles:
                     last_tile.connect(NORTH, tiles[above])
                     tiles[above].connect(SOUTH, last_tile)
-                    last_tile2.connect(NORTH, tiles2[above], ignore_dir=True)
-                    tiles2[above].connect(SOUTH, last_tile2, ignore_dir=True)
+                    last_tile2.connect(tiles2[above])
                 if before in tiles:
                     last_tile.connect(EAST, tiles[before])
                     tiles[before].connect(WEST, last_tile)
-                    last_tile2.connect(EAST, tiles2[before], ignore_dir=True)
-                    tiles2[before].connect(WEST, last_tile2, ignore_dir=True)
+                    last_tile2.connect(tiles2[before])
     last_tile.is_end = True
     last_tile2.is_end = True
 
 first_tile = (tile for tile in tiles.values() if tile.is_start).__next__()
 first_tile2 = (tile for tile in tiles2.values() if tile.is_start).__next__()
 
-# path = first_tile2.trace_path()
+for tile in tiles2.values():
+    if not tile.is_node():
+        tile.join()
+
+
+# dist, path = first_tile2.trace_path()
 # print("  ", end="")
 # for x in range(width):
 #     print(f"{x%10}", end="")
@@ -128,13 +203,15 @@ first_tile2 = (tile for tile in tiles2.values() if tile.is_start).__next__()
 #                     print("S", end="")
 #                 elif cur.is_end:
 #                     print("E", end="")
+#                 elif cur.is_node():
+#                     print("X", end="")
 #                 else:
-#                     print(cur.type, end="")
+#                     print(".", end="")
 #         else:
 #             print("#", end="")
 #     print()
 
-# print()
+# print(f"Dist: {dist}\n")
 
 # -1 to compensate double counting the starting point
 print(f"Part 1: {first_tile.find_path()-1}")
