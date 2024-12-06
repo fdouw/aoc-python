f573946dc43e37d2450b44a7bee485d0ccca4f22
Floris Douw <fdouw@hotmail.com>
2024-12-06 22:58:18 +0100

    Improve day 6 by using tuples instead of dicts

--- 2024/day06.py
+++ 2024/day06.py
@@ -1,68 +1,70 @@
 #!/usr/bin/env python
 
 from collections import defaultdict
+from typing import Tuple
 
 
-def move(pos, dir):
-    return (pos[0] + dirs[dir][0], pos[1] + dirs[dir][1])
+def move(pos: Tuple[int, int, int]):
+    return (pos[0] + dirs[pos[2]][0], pos[1] + dirs[pos[2]][1], pos[2])
+
+
+def rotate(pos, amount=1):
+    return (pos[0], pos[1], (pos[2] + amount) % 4)
 
 
 def in_area(pos):
     return 0 <= pos[0] < width and 0 <= pos[1] < height
 
 
-def trace_guard(start, dir, limit=1_000_000):
+def trace_guard(start, limit=1_000_000) -> Tuple[set, Tuple[int, int, int]]:
     pos = start
-    directions = defaultdict(set, {pos: {dir}})
+    directions = {pos}
     for _ in range(limit):
-        nxt = move(pos, dir)
+        nxt = move(pos)
         if not in_area(nxt):
             return directions, outside
-        if nxt in obstacles:
-            dir = (dir + 1) % 4
-            nxt = move(pos, dir)
-        if nxt in obstacles:
+        if (nxt[0], nxt[1]) in obstacles:
+            nxt = move(rotate(pos))
+        if (nxt[0], nxt[1]) in obstacles:
             # This rotation will be enough, because this is where we came from
-            dir = (dir + 1) % 4
-            nxt = move(pos, dir)
+            nxt = move(rotate(pos, amount=2))
         if not in_area(nxt):
             return directions, outside
-        if nxt in directions and dir in directions[nxt]:
+        if nxt in directions:
             return directions, nxt
-        directions[nxt].add(dir)
+        directions.add(nxt)
         pos = nxt
     raise Exception("Oh no too long!")
 
 
 # The order of these is important to make rotations work
 dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-outside = (-1, -1)
+outside = (-1, -1, -1)
 
-startdir = 0
 obstacles = set()
 width = 0
 height = 0
-start = (-1, -1)
+start = (-1, -1, 0)
 with open("inputs/day06", "r") as f:
     for y, line in enumerate(f.readlines()):
         height = max(height, y + 1)
         for x, c in enumerate(line):
             width = max(width, x + 1)
             if c == "^":
-                start = (x, y)
+                start = (x, y, 0)
             elif c == "#":
                 obstacles.add((x, y))
 
 
-visited, _ = trace_guard(start, startdir)
-print(f"Part 1: {len(visited)}")
+visited, _ = trace_guard(start)
+print(f"Part 1: {len(set(map(lambda t: (t[0],t[1]), visited)))}")
 
 # Part 2
 obstructions = set()
-for x, y in visited:
-    if (x, y) != start:
+for x, y, d in visited:
+    if (x, y, 0) != start:
         obstacles.add((x, y))
-        _, location = trace_guard(start, startdir)
+        _, location = trace_guard(start)
         if location != outside:
             obstructions.add((x, y))
         obstacles.remove((x, y))
