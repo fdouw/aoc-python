cffc23c2c54642ef87cc3f2826726e93560b1893
Floris Douw <fdouw@hotmail.com>
2024-12-06 22:06:24 +0100

    Add day 6 part 2

--- 2024/day06.py
+++ 2024/day06.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from collections import defaultdict
+
 
 def move(pos, dir):
     return (pos[0] + dirs[dir][0], pos[1] + dirs[dir][1])
@@ -9,65 +11,62 @@ def in_area(pos):
     return 0 <= pos[0] < width and 0 <= pos[1] < height
 
 
-def is_loop(obst, pos, dir, depth=1200):
-    p = pos
-    for _ in range(depth):
-        if p in directions and directions[p] == dir:
-            return True
-        elif not in_area(p):
-            return False
-        nxt = move(p, dir)
-        if nxt in obstacles or nxt == obst:
+def trace_guard(start, dir, limit=1_000_000):
+    pos = start
+    directions = defaultdict(set, {pos: {dir}})
+    for _ in range(limit):
+        nxt = move(pos, dir)
+        if not in_area(nxt):
+            return directions, outside
+        if nxt in obstacles:
             dir = (dir + 1) % 4
-            nxt = move(p, dir)
-        if nxt in obstacles or nxt == obst:
+            nxt = move(pos, dir)
+        if nxt in obstacles:
+            # This rotation will be enough, because this is where we came from
             dir = (dir + 1) % 4
-            nxt = move(p, dir)
-        p = nxt
-    return False
+            nxt = move(pos, dir)
+        if not in_area(nxt):
+            return directions, outside
+        if nxt in directions and dir in directions[nxt]:
+            return directions, nxt
+        directions[nxt].add(dir)
+        pos = nxt
+    raise Exception("Oh no too long!")
 
 
+# The order of these is important to make rotations work
+dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+outside = (-1, -1)
+
+startdir = 0
 obstacles = set()
 width = 0
 height = 0
+start = (-1, -1)
 with open("inputs/day06", "r") as f:
     for y, line in enumerate(f.readlines()):
         height = max(height, y + 1)
         for x, c in enumerate(line):
             width = max(width, x + 1)
             if c == "^":
-                pos = (x, y)
+                start = (x, y)
             elif c == "#":
                 obstacles.add((x, y))
 
-# The order of these is important to make rotations work
-dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-dir = 0
 
-directions = {pos: dir}
+visited, _ = trace_guard(start, startdir)
+print(f"Part 1: {len(visited)}")
+
+# Part 2
 obstructions = set()
-while True:
-    nxt = move(pos, dir)
-    if not in_area(nxt):
-        break
-    if nxt in obstacles:
-        dir = (dir + 1) % 4
-        nxt = move(pos, dir)
-    if not in_area(nxt):
-        break
-    if nxt in obstacles:
-        # This rotation will be enough, because this is where we came from
-        dir = (dir + 1) % 4
-        nxt = move(pos, dir)
-    # Part 2
-    obst = move(nxt, dir)
-    if obst not in obstacles and in_area(obst):
-        # Assume that we only have to test in a straight line
-        if is_loop(obst, nxt, (dir + 1) % 4):
-            obstructions.add(obst)
-    # Register move
-    directions[nxt] = dir
-    pos = nxt
+for x, y in visited:
+    if (x, y) != start:
+        obstacles.add((x, y))
+        _, location = trace_guard(start, startdir)
+        if location != outside:
+            obstructions.add((x, y))
+        obstacles.remove((x, y))
+print(f"Part 2: {len(obstructions)}")
 
 # print()
 # for y in range(height):
@@ -77,15 +76,14 @@ while True:
 #             print("#", end="")
 #         elif p in obstructions:
 #             print("O", end="")
-#         elif p in directions:
-#             if directions[p] in {0, 2}:
-#                 print("|", end="")
-#             else:
+#         elif p in visited:
+#             if len(visited) == 2:
+#                 print("+", end="")
+#             elif visited[p].isdisjoint({0, 2}):
 #                 print("-", end="")
+#             else:
+#                 print("|", end="")
 #         else:
 #             print(".", end="")
 #     print()
 # print()
-
-print(f"Part 1: {len(directions)}")
-print(f"Part 2: {len(obstructions)}")
