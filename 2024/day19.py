e93b44c761dcd0a7e8254264bae7b094425873c7
Floris Douw <fdouw@hotmail.com>
2024-12-19 19:37:43 +0100

    Add day 19, part 2

--- 2024/day19.py
+++ 2024/day19.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 
+from functools import cache
 import re
 
-with open("inputs/day19", "r") as f:
-    patterns, designs = f.read().split("\n\n")
-    patterns = re.compile(f"^({patterns.replace(", ","|")})+$")
-    part1 = sum(1 for design in designs.splitlines() if patterns.match(design))
 
-print(f"Part 1: {part1}")
+@cache
+def count(design):
+    if len(design) == 0:
+        return 1
+    else:
+        return sum(count(design[len(p) :]) for p in patterns if design.startswith(p))
+
+
+with open("inputs/day19", "r") as f:
+    pattern_data, designs = f.read().split("\n\n")
+    patterns = re.compile(f"^({pattern_data.replace(", ","|")})+$")
+
+possible = list(filter(lambda d: patterns.match(d), designs.splitlines()))
+print(f"Part 1: {len(possible)}")
+
+patterns = list(filter(lambda p: p in ",".join(possible), pattern_data.split(", ")))
+part2 = sum(map(count, possible))
+print(f"Part 2: {part2}")
+
+# for d in possible:
+#     print(f"{count(d):3}: {d}")
