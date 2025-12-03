65c45282f5b473b7d70a93b45e8b3d61fe0435bb
Floris Douw <fdouw@hotmail.com>
2025-12-03 19:51:12 +0100

    Add 2025 day 3, part 2. Now using recursion for both parts.

--- 2025/day03.py
+++ 2025/day03.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 
+
+def find_largest_jolt(banks: list[int], digit_count: int, start_index: int, joltage: int) -> int:
+    if digit_count == 0:
+        return joltage
+    digit_count -= 1
+
+    max_idx = start_index
+    for i in range(start_index + 1, len(banks) - digit_count):
+        if banks[i] > banks[max_idx]:
+            max_idx = i
+
+    return find_largest_jolt(banks, digit_count, max_idx + 1, 10 * joltage + banks[max_idx])
+
+
 jolts = 0
+jolts2 = 0
 with open("inputs/03.txt") as f:
     for l in f.readlines():
         digits = list(map(int, l.strip()))
-        i_max = 0
-        for i in range(1, len(digits) - 1):
-            if digits[i] > digits[i_max]:
-                i_max = i
-        j_max = i_max + 1
-        for j in range(i_max + 2, len(digits)):
-            if digits[j] > digits[j_max]:
-                j_max = j
-        # jolt = 10 * i_max + j_max
-        jolts += 10 * digits[i_max] + digits[j_max]
+        jolts += find_largest_jolt(digits, 2, 0, 0)
+        jolts2 += find_largest_jolt(digits, 12, 0, 0)
 
 print(f"Part 1: {jolts}")
+print(f"Part 2: {jolts2}")
