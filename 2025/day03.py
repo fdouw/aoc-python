bfe515fe3b308462b155aef766e4d77f5bfbc697
Floris Douw <fdouw@hotmail.com>
2025-12-04 22:32:47 +0100

    Add improvements for days 3 and 4
    Monostack for day 3 doesn't seem to make much difference.
    Checking only the (changed) rolls makes a huge difference for day 4.

--- 2025/day03.py
+++ 2025/day03.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from functools import reduce
+
 
 def find_largest_jolt(banks: list[int], digit_count: int, start_index: int, joltage: int) -> int:
     if digit_count == 0:
@@ -14,13 +16,31 @@ def find_largest_jolt(banks: list[int], digit_count: int, start_index: int, jolt
     return find_largest_jolt(banks, digit_count, max_idx + 1, 10 * joltage + banks[max_idx])
 
 
+def monostack(banks: list[int], digit_count: int) -> int:
+    stack = [0] * (digit_count + 1)
+    last_idx = -1
+    remaining = len(banks)
+    for d in banks:
+        if last_idx < digit_count:
+            last_idx += 1
+            stack[last_idx] = d
+            while last_idx + remaining > digit_count and last_idx > 0 and stack[last_idx - 1] < d:
+                stack[last_idx - 1] = d
+                last_idx -= 1
+        remaining -= 1
+    return reduce(lambda a, b: 10 * a + b, stack[:digit_count])
+
+
 jolts = 0
 jolts2 = 0
 with open("inputs/03.txt") as f:
     for l in f.readlines():
         digits = list(map(int, l.strip()))
-        jolts += find_largest_jolt(digits, 2, 0, 0)
-        jolts2 += find_largest_jolt(digits, 12, 0, 0)
+        jolts += monostack(digits, 2)
+        jolts2 += monostack(digits, 12)
+        # jolts += find_largest_jolt(digits, 2, 0, 0)
+        # jolts2 += find_largest_jolt(digits, 12, 0, 0)
+
 
 print(f"Part 1: {jolts}")
 print(f"Part 2: {jolts2}")
--- 2025/day04.py
+++ 2025/day04.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python
 
+from collections import deque
+
 rolls = set()
 with open("inputs/04.txt") as f:
-    lines = f.readlines()
-    height = len(lines)
-    width = len(lines[0].strip())
-    for y, l in enumerate(lines):
+    for y, l in enumerate(f.readlines()):
         for x, c in enumerate(l.strip()):
             if c == "@":
                 rolls.add((x, y))
 
 deltas = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
 movable = 0
-for y in range(height):
-    for x in range(width):
-        if (x, y) in rolls and sum(1 for d in deltas if (x + d[0], y + d[1]) in rolls) < 4:
-            movable += 1
+for x, y in rolls:
+    if sum(1 for d in deltas if (x + d[0], y + d[1]) in rolls) < 4:
+        movable += 1
 
 print(f"Part 1: {movable}")
 
-repeat = True
 movable = 0
-while repeat:
-    repeat = False
-    for y in range(height):
-        for x in range(width):
-            if (x, y) in rolls and sum(1 for d in deltas if (x + d[0], y + d[1]) in rolls) < 4:
-                repeat = True
-                rolls.remove((x, y))
-                movable += 1
+to_check = deque(rolls)
+while to_check:
+    x, y = to_check.popleft()
+    if (x, y) in rolls and sum(1 for d in deltas if (x + d[0], y + d[1]) in rolls) < 4:
+        rolls.remove((x, y))
+        movable += 1
+        to_check.extend((x + d[0], y + d[1]) for d in deltas)
 
 print(f"Part 2: {movable}")
